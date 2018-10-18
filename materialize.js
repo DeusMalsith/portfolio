9d3e04c1f4294b1a67fc75f8dce59e833c13d9b7
DeusMalsith <justinrashallphotography@gmail.com>
2018-10-17 19:33:10 -0700

    commit

---
+++ materialize.js
@@ -0,0 +1,6 @@
+  document.addEventListener('DOMContentLoaded', function() {
+  	console.log('Initialized!');
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+
+  });
